7e9fa5a8c368b5f6b1641dfc932264e1f393b5a9
fboeller <fabianboeller@googlemail.com>
2020-05-26 21:56:48 +0200

    Rename active to lastClickTarget

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -2,42 +2,42 @@
   <div id="timeline h-64">
     <TimelineBlock
       size="w-4/12"
-      v-bind:active="active === 0"
-      v-on:click="active = 0"
+      v-bind:active="lastClickTarget === 0"
+      v-on:click="lastClickTarget = 0"
     />
     <div class="clear-both"></div>
     <TimelineBlock
       size="w-4/12"
-      v-bind:active="active === 1"
-      v-on:click="active = 1"
+      v-bind:active="lastClickTarget === 1"
+      v-on:click="lastClickTarget = 1"
     />
     <div class="clear-both"></div>
     <div class="indent w-4/12"></div>
     <TimelineBlock
       size="w-5/12"
-      v-bind:active="active === 2"
-      v-on:click="active = 2"
+      v-bind:active="lastClickTarget === 2"
+      v-on:click="lastClickTarget = 2"
     />
     <div class="clear-both"></div>
     <div class="indent w-5/12"></div>
     <TimelineBlock
       size="w-1/12"
-      v-bind:active="active === 3"
-      v-on:click="active = 3"
+      v-bind:active="lastClickTarget === 3"
+      v-on:click="lastClickTarget = 3"
     />
     <div class="clear-both"></div>
     <div class="indent w-7/12"></div>
     <TimelineBlock
       size="w-2/12"
-      v-bind:active="active === 4"
-      v-on:click="active = 4"
+      v-bind:active="lastClickTarget === 4"
+      v-on:click="lastClickTarget = 4"
     />
     <div class="clear-both"></div>
     <div class="indent w-9/12"></div>
     <TimelineBlock
       size="w-3/12"
-      v-bind:active="active === 5"
-      v-on:click="active = 5"
+      v-bind:active="lastClickTarget === 5"
+      v-on:click="lastClickTarget = 5"
     />
   </div>
 </template>
@@ -52,7 +52,7 @@ export default {
   },
   data: function() {
     return {
-      active: 0,
+      lastClickTarget: 0,
     };
   },
 };
