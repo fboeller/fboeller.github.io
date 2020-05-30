f90d8f8a424a5f93600509ecc4a3e0752a4149f8
fboeller <fabianboeller@googlemail.com>
2020-05-30 21:34:18 +0200

    Add hover effect to timeline block

--- src/components/TimelineBlock.vue
+++ src/components/TimelineBlock.vue
@@ -19,6 +19,11 @@ export default {
 </script>
 
 <style scoped>
+.block:hover {
+  @apply shadow-xl;
+  transform: translateY(-5px)
+}
+
 .active {
   @apply bg-blue shadow-lg;
   transform: translateY(-2px);
