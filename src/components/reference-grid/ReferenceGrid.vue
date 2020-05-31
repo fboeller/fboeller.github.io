aebc6a2f995a4543ce64d6f1c5160c99465b7aea
fboeller <fabianboeller@googlemail.com>
2020-06-01 01:06:47 +0200

    Add second medium article

--- src/components/reference-grid/ReferenceGrid.vue
+++ src/components/reference-grid/ReferenceGrid.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
     <MediumArticle :article="articles[0]" />
+    <MediumArticle :article="articles[1]" />
   </div>
 </template>
 
