601e60ee4773a1b2193e156d04976453aac09a4f
fboeller <fabianboeller@googlemail.com>
2020-05-31 21:47:29 +0200

    Add links to tools

--- src/components/Tool.vue
+++ src/components/Tool.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="h-8 w-32">
+  <a :href="tool.url" target="_blank" class="h-8 w-32">
     <div class="h-8 w-8 inline-block align-middle" :class="tool.imageClasses">
       <img :src="require(`@/assets/${tool.image}`)" class="h-full w-full" />
     </div>
     <span class="ml-2">{{ tool.text }}</span>
-  </div>
+  </a>
 </template>
 
 <script>
