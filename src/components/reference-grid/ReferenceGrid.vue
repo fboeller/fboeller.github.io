5ba58fd4e4667664b6a53194342f3555d6c8a43e
fboeller <fabianboeller@googlemail.com>
2020-06-01 10:45:42 +0200

    Use v-for

--- src/components/reference-grid/ReferenceGrid.vue
+++ src/components/reference-grid/ReferenceGrid.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-    <ReferenceElement :reference="references[0]" />
-    <ReferenceElement :reference="references[1]" />
-    <ReferenceElement :reference="references[2]" />
+    <ReferenceElement
+      v-for="reference in references"
+      :key="reference.title"
+      :reference="reference"
+    />
   </div>
 </template>
 
