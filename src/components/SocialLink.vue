2be8c4608bf56388cd6c0060181db567140416ab
fboeller <fabianboeller@googlemail.com>
2020-06-03 19:24:26 +0200

    Reduce size

--- src/components/SocialLink.vue
+++ src/components/SocialLink.vue
@@ -3,7 +3,7 @@
     class="rounded-lg pl-1 pr-4 py-1 bg-blue shadow-md cursor-pointer inline-block hover:border-opacity-100 border-opacity-0 border border-gray-300 border-solid"
     :href="link.url"
   >
-    <div class="h-12 w-12 inline-block align-middle">
+    <div class="h-8 w-8 inline-block align-middle">
       <img :src="require(`@/assets/icons/${link.icon}`)" class="h-full w-full rounded-lg" />
     </div>
     <p class="ml-3 inline-block">{{ link.text }}</p>
