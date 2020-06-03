b2a05f6bbcb7b5e3c8ebff755bee27b020b0b56f
fboeller <fabianboeller@googlemail.com>
2020-06-03 19:42:23 +0200

    Make it work on mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="flex flex-wrap justify-between h-20 sm:h-32">
+  <header class="flex flex-wrap justify-between">
     <div class="flex flex-no-wrap">
-      <div class="w-20 sm:w-32">
+      <div class="w-20 sm:w-32 h-20 sm:h-32">
         <img :src="require('@/assets/formal_square_320.jpg')" class="rounded-full" />
       </div>
       <div class="ml-4 sm:ml-8 mt-3 sm:mt-8">
@@ -9,7 +9,7 @@
         <h2 class="text-sm sm:text-lg text-gray-300">Senior Software Engineer</h2>
       </div>
     </div>
-    <div class="my-auto w-30 grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
+    <div class="mt-4 w-full lg:w-auto lg:my-auto w-30 grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
       <SocialLink v-for="link of socialLinks" :key="link.url" :link="link" />
     </div>
   </header>
