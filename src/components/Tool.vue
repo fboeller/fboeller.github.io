9aae13b8b651c36bfb941d250047cf8fb2ab1d5e
fboeller <fabianboeller@googlemail.com>
2021-03-16 21:23:00 +0100

    Add alt text to tools

--- src/components/Tool.vue
+++ src/components/Tool.vue
@@ -2,6 +2,7 @@
   <a :href="tool.url" target="_blank" class="relative inline-block h-8 w-32">
     <img
       :src="require(`@/assets/${tool.image}`)"
+      :alt="tool.text + ' Logo'"
       class="inline h-8 w-8 rounded-lg"
       :class="tool.imageClasses"
     />
