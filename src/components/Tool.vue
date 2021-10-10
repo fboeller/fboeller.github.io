24bab5e4247d096145fc62199f886225c8b022b7
fboeller <fabianboeller@googlemail.com>
2021-10-10 20:03:52 +0200

    Add default for logos

--- src/components/Tool.vue
+++ src/components/Tool.vue
@@ -6,11 +6,27 @@
     class="relative inline-block h-8 w-32"
   >
     <img
+      v-if="tool.image"
       :src="require(`@/assets/${tool.image}`)"
       :alt="tool.text + ' Logo'"
       class="inline h-8 w-8 rounded-lg"
       :class="tool.imageClasses"
     />
+    <span
+      v-else
+      class="
+        h-8
+        w-8
+        text-lg
+        font-bold font-serif
+        leading-8
+        flex
+        justify-center
+        rounded-lg
+        bg-blue-lighter
+      "
+      >{{ tool.text.charAt(0).toUpperCase() }}</span
+    >
     <p class="absolute">{{ tool.text }}</p>
   </a>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ module.exports = {
   theme: {
     fontFamily: {
       display: ["Montserrat", "sans-serif"],
-      body: ["Montserrat", "sans-serif"]
+      body: ["Montserrat", "sans-serif"],
+      serif: ["ui-serif", "Garamond"],
     },
     extend: {
       colors: {
         blue: {
           darker: "#044965",
           default: "#05668D",
-          lighter: "#018293"
-        }
+          lighter: "#018293",
+        },
       },
       spacing: {
         "72": "18rem",
@@ -19,7 +20,7 @@ module.exports = {
         "84": "21rem",
         "90": "22.5rem",
         "96": "24rem",
-        "168": "36rem"
+        "168": "36rem",
       },
       translate: {
         "100": "100vw",
@@ -35,10 +36,10 @@ module.exports = {
         "-400": "-400vw",
         "-500": "-500vw",
         "-600": "-600vw",
-        "-700": "-700vw"
-      }
-    }
+        "-700": "-700vw",
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
